0fa6feaf321418c8858ce3fa1ca0fa1beb5de0ae
Natalia Patiño <54645217+Natalia-SP@users.noreply.github.com>
2021-08-30 16:22:20 -0500

    Create 11611 Watermelon.py

---
+++ Nivel Básico: Introducción a la programación/Python/11611 Watermelon.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def _main() -> None:
+  # TODO: fixme.
+  w = int(input())
+  if w > 2 and w % 2 == 0:
+    print('SI\n')
+  else:
+    print('NO\n')
+  pass
+
+if __name__ == '__main__':
+  _main()
